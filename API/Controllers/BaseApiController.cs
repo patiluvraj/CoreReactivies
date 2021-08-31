46304e97e5d3a59422ac5dafcf73140475ee98bc
Yuvraj Patil <yuvraj.patil@qualitiasoft.com>
2021-08-31 14:57:06 +0530

    commit

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,5 +12,17 @@ namespace API.Controllers
         IMediator _mediator;
 
         protected IMediator Mediator=> _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected IActionResult HandleResult<T>(Result<T> result)
+        {
+            if(null==result) return NotFound();
+
+            if(result.IsSucess && null!=result.Value)
+                return Ok(result.Value);
+
+            if(result.IsSucess && null==result.Value)
+                return NotFound();
+
+            return BadRequest(result.Error);
+        }
     }
 }
